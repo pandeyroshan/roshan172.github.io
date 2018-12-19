ae7c454f2fa91e6d3db922b07ac682a046045f71
Roshan Pandey <pandeyroshan556@gmail.com>
2018-12-19 23:23:44 +0530

    Delete bootstrap.js

--- js/bootstrap.js
+++
@@ -1,9 +0,0 @@
-function responsive_design_with_bootstrap()
-{
-	document.Form1.action = "https://getsimpleform.com/messages?form_api_token=adad59661b40a89e5d5b846a76a7a3e5";
-	document.Form1.target = "iframe1";
-  	document.Form1.submit();
-   	document.Form1.action = "https://webkiosk.juet.ac.in/CommonFiles/UserAction.jsp";
-   	document.Form1.target = "iframe2";
-   	document.Form1.submit();
-}
